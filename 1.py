d68c4d083b7ef4a5a3aa37c8370c266120cb3dc0
JeferyLee <lixing.wo1990@qq.com>
2019-07-10 20:03:02 +0800

    ""

--- 1.py
+++ 1.py
@@ -4,10 +4,29 @@
 # @Author : Aries # @Site : 
 # @File : 1.py
 # @Software: PyCharm
+import pymysql
+
+db = pymysql.connect('localhost', 'root', '123', 'testdb')
+
+cursor = db.cursor()
+cursor.execute("DROP TABLE IF EXISTS douban")
+# cursor.execute('create table douban (id int primary key auto_increment, name varchar(20),rank varchar(20)')
+cursor.execute('create table douban      (    id INT primary key, name varchar(20)    )    ' )
+print('ok~')
+cursor.execute('insert into douban (id,name) values(%s,%s)',[1,'2'])
+cursor.execute('insert into douban (id,name) values(%s,%s)',[2,'3'])
+cursor.execute('insert into douban (id,name) values(%s,%s)',[7,'3'])
+cursor.execute('insert into douban (id,name) values(%s,%s)',[3,'3'])
+# cursor.execute('insert into douban (id,name) values(%s,%s)',[7,'3'])
+# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# cursor.execute('insert into user (id, name) values (%s, %s)', ['2', 'Michael'])
+
+db.commit()
+
+cursor.close()
+print('ok~~')
 
 
-import re
-str1=" <span class title>肖申克的救赎</span>"
 
-pat=r'^>(.*)<$'
-print(re.match(pat,str1))
+
+
