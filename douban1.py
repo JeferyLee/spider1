12be69ecce7e1d5264b222b1a73df1aeee7ff00b
JeferyLee <lixing.wo1990@qq.com>
2019-07-10 20:02:56 +0800

    ""

--- douban1.py
+++ douban1.py
@@ -12,7 +12,7 @@ import  pymysql
 #
 list_rank=[]  #用于存放排名
 list_name=[]  #用于存放电影名
-list_descrip=[]  #用于存放电影简介
+list_quote=[]  #用于存放电影简介
 list_site=[]  #用于存放电影网址
 url="https://movie.douban.com/top250"
 res=urlopen(url)
@@ -29,6 +29,11 @@ for each in bs_pic:
     list_rank.append(each.find('em').text)
 print(list_rank)
 
+bs_quote=bs_ol.findAll('p',class_='quote')
+for each in bs_quote:
+    list_quote.append(each.find('span',class_='inq').text)
+print(list_quote)
+
 
 #mysql
 
@@ -37,20 +42,22 @@ def create():
     db=pymysql.connect('localhost','root','123','testdb')
     cursor=db.cursor()
     cursor.execute("DROP TABLE IF EXISTS douban")
-    cursor.execute('create table douban (id varchar(20) primary key, name varchar(20))')
+    cursor.execute('create table douban (id int primary key, name varchar(20), quote varchar(200))      ')
     db.close()
     print('create table Successfully!')
 def  insert(value):
     db=pymysql.connect('localhost','root','123','testdb')
     cursor=db.cursor()
-    cursor.execute('insert into douban (id,name) values(%s,%s)',value)
+    cursor.execute('insert into douban (id,name,quote) values(%s,%s,%s)',value)
     db.commit()
     db.close()
     print('ok')
 
 create()
-value=['a','b']
-insert(value)
+for i in range(len(list_name)):
+    value=(int(list_rank[i]),list_name[i],list_quote[i])
+    insert(value)
+print('ok..')
 
 
 
