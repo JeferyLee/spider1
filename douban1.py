ca51306ea6f3a17373c47e09fe8894443efa81ae
JeferyLee <lixing.wo1990@qq.com>
2019-07-10 18:11:10 +0800

    ""

--- douban1.py
+++ douban1.py
@@ -8,7 +8,8 @@
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
-
+import  pymysql
+#
 list_rank=[]  #用于存放排名
 list_name=[]  #用于存放电影名
 list_descrip=[]  #用于存放电影简介
@@ -18,6 +19,50 @@ res=urlopen(url)
 data=res.read().decode('utf-8')
 bs1=BeautifulSoup(data,'lxml')
 bs_ol=bs1.find('ol',class_='grid_view')
+bs_hd=bs_ol.findAll('div',class_='hd')
+for each in bs_hd:
+    list_name.append(each.find('span',class_='title').text)
+print(list_name)
+
+bs_pic=bs_ol.findAll('div',class_='pic')
+for each in bs_pic:
+    list_rank.append(each.find('em').text)
+print(list_rank)
+
+
+#mysql
+
+
+def create():
+    db=pymysql.connect('localhost','root','123','testdb')
+    cursor=db.cursor()
+    cursor.execute("DROP TABLE IF EXISTS douban")
+    cursor.execute('create table douban (id varchar(20) primary key, name varchar(20))')
+    db.close()
+    print('create table Successfully!')
+def  insert(value):
+    db=pymysql.connect('localhost','root','123','testdb')
+    cursor=db.cursor()
+    cursor.execute('insert into douban (id,name) values(%s,%s)',value)
+    db.commit()
+    db.close()
+    print('ok')
+
+create()
+value=['a','b']
+insert(value)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
