8455b63191574c284b867eaa1dd3d8c29b0d9e07
JeferyLee <lixing.wo1990@qq.com>
2019-07-11 18:13:28 +0800

    ""

--- 1.py
+++ 1.py
@@ -6,26 +6,25 @@
 # @Software: PyCharm
 import pymysql
 
-db = pymysql.connect('localhost', 'root', '123', 'testdb')
-
-cursor = db.cursor()
-cursor.execute("DROP TABLE IF EXISTS douban")
-# cursor.execute('create table douban (id int primary key auto_increment, name varchar(20),rank varchar(20)')
-cursor.execute('create table douban      (    id INT primary key, name varchar(20)    )    ' )
-print('ok~')
-cursor.execute('insert into douban (id,name) values(%s,%s)',[1,'2'])
-cursor.execute('insert into douban (id,name) values(%s,%s)',[2,'3'])
-cursor.execute('insert into douban (id,name) values(%s,%s)',[7,'3'])
-cursor.execute('insert into douban (id,name) values(%s,%s)',[3,'3'])
+# db = pymysql.connect('localhost', 'root', '123', 'testdb')
+#
+# cursor = db.cursor()
+# cursor.execute("DROP TABLE IF EXISTS douban")
+# # cursor.execute('create table douban (id int primary key auto_increment, name varchar(20),rank varchar(20)')
+# cursor.execute('create table douban      (    id INT primary key, name varchar(20)    )    ' )
+# print('ok~')
+# cursor.execute('insert into douban (id,name) values(%s,%s)',[1,'2'])
+# cursor.execute('insert into douban (id,name) values(%s,%s)',[2,'3'])
 # cursor.execute('insert into douban (id,name) values(%s,%s)',[7,'3'])
-# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-# cursor.execute('insert into user (id, name) values (%s, %s)', ['2', 'Michael'])
+#
+# db.commit()
+#
+# cursor.close()
+# print('ok~~')
 
-db.commit()
 
-cursor.close()
-print('ok~~')
-
+page=2*25+1
+print(page,type(page))
 
 
 
